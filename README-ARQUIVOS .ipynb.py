b8bbae73fdd1992a893b41c739b4badf9f78f0c6
SamuelHericles <samuel.herilces@alu.ufc.br>
2020-08-03 10:17:01 -0300

    Criando um arquivo .ipynb

---
+++ README-ARQUIVOS .ipynb.py
@@ -0,0 +1,3 @@
+1. Procure sobre colab google
+2. Abra o arquivo Trabalho_01_Reconhecimento_de_padroes.ipynb no colab
+3. Execute
---
+++ README-ARQUIVOS .py.py
@@ -0,0 +1,13 @@
+1. Instale o python3.8;
+
+2. Após instalar o python instale o pip, gerenciador de pacotes do python;
+
+3. Após instalar o pip, instale o pandas, no terminal windows ou linux:
+				pip install pandas
+	se for windows, abra o terminal como administrador.
+	
+4. Após instalar o pip, instale o pandas, no terminal windows ou linux:
+			   pip install numpy
+	se for windows, abra o terminal como administrador.
+
+5. Após isso tudo tente executar o código em alguma IDE.
